d45d6e8f927037876a237f418879f503991106c3
TerkumaAmos <agberterkuma2020@gmail.com>
2025-03-21 11:23:40 +0100

    new

--- lib/splash.dart
+++ lib/splash.dart
@@ -7,12 +7,38 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF061F37),
-   body: Column(
-     children: [
-       Image(image: AssetImage("assetName"))
-     ],
+      // backgroundColor: Color(0xFF061F37),
+   body: SafeArea(
+     child: Padding(
+       padding: const EdgeInsets.only(top: 20.0),
+       child: Row(
+         mainAxisAlignment: MainAxisAlignment.center,
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Image(
+               image: AssetImage(
+                   "assets/Logo1.png",
+               ),
+
+           ),
+           SizedBox(width: 32),
+           RotatedBox(quarterTurns: 1,
+               child: Text("Eye . Buddy",
+                 style: TextStyle(
+                     fontSize: 60,
+                   fontWeight: FontWeight.bold,
+                   color: Color(0xFFD9D9D9)
+                 ),
+               ),
+           )
+
+         ],
+       ),
+     ),
+
    ),
     );
   }
 }
+
+
